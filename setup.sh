de92630209a0bf1fc075ff9b75da1a5bfe9f8658
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-05-17 04:22:34 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ wget https://raw.githubusercontent.com/Raydinzzzzz/Autoscript/main/ins-vt.sh && 
 #wget https://raw.githubusercontent.com/Raydinzzzzz/Autoscript/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
 wget -O set-br.sh https://raw.githubusercontent.com/Raydinzzzzz/Autoscript/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 #install trojan-go
-wget https://raw.githubusercontent.com/Mahfud2128/mamam/main/ins-trgo.sh && dos2unix ins-trgo.sh && chmod +x ins-trgo.sh && ./ins-trgo.sh
+#wget https://raw.githubusercontent.com/Mahfud2128/mamam/main/ins-trgo.sh && dos2unix ins-trgo.sh && chmod +x ins-trgo.sh && ./ins-trgo.sh
 #log
 wget https://raw.githubusercontent.com/Raydinzzzzz/Autoscript/main/log.sh && dos2unix log.sh && chmod +x log.sh && ./log.sh
 rm -f /root/ssh-vpn.sh
