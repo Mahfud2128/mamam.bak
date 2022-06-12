e0be271b5ff7d0b4b112ce1c6b5d6ebc81d825a5
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-12 15:19:42 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -81,6 +81,7 @@ echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
 echo "   - Stunnel4                : 8443, 902"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
+echo "   - WebSocket/WS Ovpn       : 2086"  | tee -a log-install.txt
 echo "   - WebSocket/WS Non SSL    : 2082"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 81"  | tee -a log-install.txt
