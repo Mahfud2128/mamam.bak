00696e17239c597b478b4bfe46ed88ab4864c784
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-12 07:42:16 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -40,7 +40,7 @@ wget -O set-br.sh https://raw.githubusercontent.com/Mahfud2128/mamam/main/set-br
 #install xray-grpc
 wget https://raw.githubusercontent.com/Mahfud2128/mamam/main/xray-grpc.sh && chmod +x xray-grpc.sh && ./xray-grpc.sh
 #log
-wget https://raw.githubusercontent.com/Mahfud2128/mamam/main/log.sh && dos2unix log.sh && chmod +x log.sh && ./log.sh
+#wget https://raw.githubusercontent.com/Mahfud2128/mamam/main/log.sh && dos2unix log.sh && chmod +x log.sh && ./log.sh
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
 rm -f /root/ss.sh
@@ -49,8 +49,8 @@ rm -f /root/wg.sh
 rm -f /root/ins-vt.sh
 rm -f /root/ipsec.sh
 rm -f /root/set-br.sh
-rm -f /root/ins-trgo.sh
-rm -f /root/log.sh
+#rm -f /root/ins-trgo.sh
+#rm -f /root/log.sh
 rm -f /root/xray-grpc.sh
 
 cat <<EOF> /etc/systemd/system/autosett.service
@@ -72,6 +72,52 @@ history -c
 echo "1.2" > /home/ver
 clear
 echo " "
+echo "===============================-{ Autoscript Premium }-=========================" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
+echo "   - Stunnel4                : 902, 777"  | tee -a log-install.txt
+echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
+echo "   - WebSocket/WS SSL        : 8443"  | tee -a log-install.txt
+echo "   - WebSocket/WS Non SSL    : 2082"  | tee -a log-install.txt
+echo "   - WebSocket/WS Ovpn       : 2086"  | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 81"  | tee -a log-install.txt
+echo "   - Wireguard               : 7070"  | tee -a log-install.txt
+echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
+echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
+echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
+echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
+echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
+echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
+echo "   - V2RAY Vmess TLS         : 443"  | tee -a log-install.txt
+echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
+echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
+echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
+echo "   - Trojan                  : 2087"  | tee -a log-install.txt
+echo "   - Vmess GRPC              : 2053"  | tee -a log-install.txt
+echo "   - Vless GRPC              : 2096"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]" | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "==========================-[ AUTOSCRIPT PREMIUM ]-===========================" | tee -a log-install.txt
 echo "Installation has been completed!!"
 echo " "
  echo " Reboot 10 Sec"
