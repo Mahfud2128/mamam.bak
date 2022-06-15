565a0252499b0c9179c77e32e2ac079d1a9ad9f1
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-15 12:45:51 +0700

    set-backupbot.sh

---
+++ set-bot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+neofetch
+echo -e "Silahkan cek CHAT ID anda di tele @get_id_bot" | lolcat
+echo -e "lalu klik start atau cmd /my_id" | lolcat
+echo -e ""
+ID=$(cat backupbot | grep CHATID=  | sed 's/"//g' | sed 's/CHATID//g' | sed 's/=//g')
+
+read -p "Masukkan Chat ID Telegram Anda=" idbaru
+
+sed -i "s/$ID/$idbaru/g" /root/backupbot
+
+echo -e ""
+echo -e "kemudian cari Bot @GDStore_bot" | lolcat
+echo -e "lalu start" | lolcat
