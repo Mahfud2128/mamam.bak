79744b5a868b158d4bbaae770edbf92669ab05a3
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-18 00:20:53 +0700

    Update xp.sh

--- xp.sh
+++ xp.sh
@@ -150,6 +150,20 @@ sed -i "/^### $user $exp/,/^},{/d" /etc/xray/vmessgrpc.json
 sed -i "/^### $user $exp/,/^},{/d" /etc/xray/vlessgrpc.json
 rm -f /etc/v2ray/$user-tls.json
 fi
+done
 systemctl restart vmess-grpc
 systemctl restart vless-grpc
+data=( `cat /etc/trojan-go/config.json | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/trojan-go/config.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^### $user $exp/,/^},{/d" /etc/trojan-go/config.json
+fi
+done
+systemctl restart trojan-go
 delete
