b1673051dfc9c9e9053a10558c624c0775566f75
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-20 01:52:57 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -166,7 +166,7 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 
 # install
 apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
-wget -q -O /root/.profile https://raw.githubusercontent.com/Mahfud2128/mamam/main/profile.sh chmod +x /root/.profile
+wget -q -O /root/.profile https://raw.githubusercontent.com/Mahfud2128/mamam/main/profile.sh && chmod +x /root/.profile
 
 # install webserver
 apt -y install nginx
