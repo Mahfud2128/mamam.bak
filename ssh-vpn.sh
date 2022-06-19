013859439d92c2ea133914f9a2fbe0f03d228dff
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-19 20:31:15 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -156,6 +156,7 @@ apt-get remove --purge exim4 -y
 # install wget and curl
 apt -y install wget curl
 apt -y install python
+apt install boxes -y
 
 # set time GMT +8
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
