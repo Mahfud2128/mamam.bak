80d42bf6324631d11e428853fddb128489e559c1
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-21 18:22:16 +0700

    Update set-backupbot.sh

--- set-backupbot.sh
+++ set-backupbot.sh
@@ -61,12 +61,13 @@ neofetch
 echo -e "Silahkan cek CHAT ID anda di tele @get_id_bot" | lolcat
 echo -e "lalu klik start atau ketik /my_id" | lolcat
 echo -e ""
-ID=$(cat /usr/bin/backupbot | grep CHATID=  | sed 's/"//g' | sed 's/CHATID//g' | sed 's/=//g')
+ID=$(cat /root/.config/idtele
 
 read -p "Masukkan Chat ID Telegram Anda=" idbaru
 
-sed -i "s/$ID/$idbaru/g" /usr/bin/backupbot
+sed -i "s/$ID/$idbaru/g" /root/.config/idtele
 
 echo -e ""
-echo -e "Sebelum backup, cari Bot @GDStore_bot" | lolcat
+echo -e "Sebelum Backup, Cari Bot @GDStore_bot" | lolcat
+echo -e "Dan Cari Bot @GDnotif_bot Untuk Menerima Notifikasi Create User | lolcat
 echo -e "lalu start" | lolcat
