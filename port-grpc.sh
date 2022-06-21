a22cebfbbe8e54d350f375fbf604c2a60fdef9c8
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-22 02:47:49 +0700

    Add files via upload

---
+++ port-grpc.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+BURIQ () {
+    curl -sS http://lino.griyadigital.xyz:81/ipvps > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS http://lino.griyadigital.xyz:81/ipvps | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS http://lino.griyadigital.xyz:81/ipvps | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+if [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
+#Port Changer Vmess
+
+# Color
+RED="\e[1;31m"
+GREEN="\e[0;32m"
+NC="\e[0m"
+
+# Validate Your IP Address
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+clear
+
+tls="$(cat ~/log-install.txt | grep -i VmessGRPC | cut -d: -f2|sed 's/ //g')"
+none="$(cat ~/log-install.txt | grep -i VlessGRPC | cut -d: -f2|sed 's/ //g')"
+echo -e "======================================"
+echo -e "         XRay GRPC Port Changer"
+echo -e ""
+echo -e "     [1]  Change Port XRay VMess GRPC ${RED}$tls${NC}"
+echo -e "     [2]  Change Port XRay VLess GRPC ${RED}$none${NC}"
+echo -e "     [x]  Exit"
+echo -e ""
+echo -e "======================================"
+echo -e ""
+read -p "     Select From Options [1-2 or x] :  " port
+echo -e ""
+case $port in
+1)
+read -p "Type New Port For XRay VMess GRPC: " tls1
+if [ -z $tls1 ]; then
+echo "Please Input Port"
+exit 0
+fi
+cek=$(netstat -nutlp | grep -w $tls1)
+if [[ -z $cek ]]; then
+sed -i "s/$tls/$tls1/g" /etc/xray/vmessgrpc.json
+sed -i "s/   - VmessGRPC               : $tls/   - VmessGRPC               : $tls1/g" /root/log-install.txt
+iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport $tls -j ACCEPT
+iptables -D INPUT -m state --state NEW -m udp -p udp --dport $tls -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport $tls1 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport $tls1 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save > /dev/null
+netfilter-persistent reload > /dev/null
+systemctl restart vmess-grpc > /dev/null
+clear
+echo -e "${GREEN}Succesfully Changed XRay VMess GRPC Port To $tls1${NC}"
+else
+echo -e "${RED}Error ! ${NC}Port $none1 Is Already Used"
+fi
+;;
+2)
+read -p "Type New Port For XRay VLess GRPC : " none1
+if [ -z $none1 ]; then
+echo "Please Input Port"
+exit 0
+fi
+cek=$(netstat -nutlp | grep -w $none1)
+if [[ -z $cek ]]; then
+sed -i "s/$none/$none1/g" /etc/xray/vlessgrpc.json
+sed -i "s/   - VlessGRPC               : $none/   - VlessGRPC               : $none1/g" /root/log-install.txt
+iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport $none -j ACCEPT
+iptables -D INPUT -m state --state NEW -m udp -p udp --dport $none -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport $none1 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport $none1 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save > /dev/null
+netfilter-persistent reload > /dev/null
+systemctl restart vless-grpc > /dev/null
+clear
+echo -e "${GREEN}Succesfully Change XRay VLess GRPC Port To $none1${NC}"
+else
+echo -e "${RED}Error ! ${NC}Port $none1 Is Already Used"
+fi
+;;
+x)
+exit
+menu
+;;
+*)
+echo "Please enter an correct number"
+;;
+esac
