6f2e1c6f15ddfee5f69e6627d347f339f496f435
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-22 00:50:07 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -57,7 +57,8 @@ red "Permission Denied!"
 exit 0
 fi
 
-echo ""
+clear
+echo " "
 echo "===============================-{ Autoscript Premium }-=========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
@@ -65,26 +66,18 @@ echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 902, 777"  | tee -a log-install.txt
+echo "   - Stunnel4                : 8443, 902"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
-echo "   - WebSocket/WS SSL        : 8443"  | tee -a log-install.txt
-echo "   - WebSocket/WS Non SSL    : 2082"  | tee -a log-install.txt
 echo "   - WebSocket/WS Ovpn       : 2086"  | tee -a log-install.txt
-echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - WebSocket/WS Non SSL    : 2082"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 81"  | tee -a log-install.txt
-echo "   - Wireguard               : 7070"  | tee -a log-install.txt
-echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
-echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
-echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
-echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
-echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
-echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
 echo "   - V2RAY Vmess TLS         : 443"  | tee -a log-install.txt
 echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
 echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
-echo "   - Trojan                  : 2087"  | tee -a log-install.txt
+echo "   - TrojanGFW               : 2087"  | tee -a log-install.txt
+echo "   - TrojanGo                : 2095"  | tee -a log-install.txt
 echo "   - Vmess GRPC              : 2053"  | tee -a log-install.txt
 echo "   - Vless GRPC              : 2096"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
@@ -104,3 +97,4 @@ echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "==========================-[ AUTOSCRIPT PREMIUM ]-===========================" | tee -a log-install.txt
+echo "Installation has been completed!!"
