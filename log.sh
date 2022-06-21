ee83000e862a8f34ec40355569a8ed246626f8ca
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-22 01:22:03 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -58,43 +58,6 @@ exit 0
 fi
 
 clear
-echo " "
-echo "===============================-{ Autoscript Premium }-=========================" | tee -a log-install.txt
-echo "" | tee -a log-install.txt
-echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
-echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 8443, 902"  | tee -a log-install.txt
-echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
-echo "   - WebSocket/WS Ovpn       : 2086"  | tee -a log-install.txt
-echo "   - WebSocket/WS Non SSL    : 2082"  | tee -a log-install.txt
-echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
-echo "   - Nginx                   : 81"  | tee -a log-install.txt
-echo "   - V2RAY Vmess TLS         : 443"  | tee -a log-install.txt
-echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
-echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
-echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
-echo "   - TrojanGFW               : 2087"  | tee -a log-install.txt
-echo "   - TrojanGo                : 2095"  | tee -a log-install.txt
-echo "   - Vmess GRPC              : 2053"  | tee -a log-install.txt
-echo "   - Vless GRPC              : 2096"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
-echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
-echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
-echo "   - IPtables                : [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
-echo "   - IPv6                    : [OFF]" | tee -a log-install.txt
-echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
-echo "   - Autobackup Data" | tee -a log-install.txt
-echo "   - Restore Data" | tee -a log-install.txt
-echo "   - Auto Delete Expired Account" | tee -a log-install.txt
-echo "   - Full Orders For Various Services" | tee -a log-install.txt
-echo "   - White Label" | tee -a log-install.txt
-echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "==========================-[ AUTOSCRIPT PREMIUM ]-===========================" | tee -a log-install.txt
-echo "Installation has been completed!!"
+./.profile
+cat /root/log-install.txt
+echo ""
