d12526966584bfc2bf2284776b0f057c9837422e
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-06-25 01:44:31 +0700

    Delete teslagi.sh

--- teslagi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CHATID="5325141986"
-KEY="5282675654:AAG7hJhGrbXWB2Q9jYbbh01AHwfl6nV96Hk"
-TIME="10"
-URL="https://api.telegram.org/bot$KEY/sendMessage"
-TEXT="https://!---
-curl -s --max-time $TIME -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT" $URL >/dev/null
-
